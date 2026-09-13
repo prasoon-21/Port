93b9da86da9cb9a8339bdb827a0c5f3219b8ac47
prasoon-21 <prasoonmishr.21@gmail.com>
2026-09-14 04:09:16 +0530

    chore(config): update personal branding, site metadata, and configuration

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -18,7 +18,10 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
     >
       <defs>
         <clipPath id={clipId}>
-          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L19.4 17l4-9L19 0h6.5a6 6 0 0 1 5.2 3.1L39.5 19 35 29 24.5 10 16 29 0 0Zm46.7 2.8A2 2 0 0 0 45 0h-7l5.5 10 3.2-7.2Z" />
+          <path
+            clipRule="evenodd"
+            d="M4 0h20a10 10 0 0 1 10 10c0 5.5-4.5 10-10 10H14v9H4V0Zm10 6v8h10a4 4 0 0 0 0-8H14Zm26-6h6l-6 13h-6l6-13Z"
+          />
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
